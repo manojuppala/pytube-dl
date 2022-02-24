3615c9a6c0664163cb70fbde29f13b29cd03c2a9
manoj uppala <uppala0863@gmail.com>
2022-02-24 23:12:25 +0530

    used ttk themes

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-# youtube_downloader
+# pytube-dl
 
-this python application can be used to download youtube videos , thumbnails and descriptions.<br>
-
-![](assets/application1.png)
-![](assets/application2.png)
-
-i know GUI is horrible i'll improve it. feel free to contribute if interested.
+this python application can be used to download youtube videos , thumbnails and descriptions.
--- downloader.py
+++ downloader.py
@@ -1,3 +1,4 @@
+import os
 from PIL import ImageTk
 import PIL.Image
 from tkinter import *
@@ -6,11 +7,25 @@ from tkinter import ttk
 from pytube import YouTube
 import requests
 import shutil
+from ttkthemes import themed_tk as tk
 
-dwnld = Tk()
-dwnld.title("youtube downloader")
+dwnld = tk.ThemedTk()
+dwnld.get_themes()
+dwnld.set_theme('clam')
+dwnld.title("pytube-dl")
 dwnld.geometry('600x400')
 dwnld.configure(bg='white')
+
+directory = os.path.expanduser('~')+r'/Downloads/'
+
+if not os.path.exists(directory): os.mkdir(directory)
+
+style = ttk.Style()
+style.configure('TButton', font=('Helvetica', 11), background='#32a852',foreground='white')
+style.map('TButton', background=[('active', '#32a86d')])
+style.configure('TLabel', font=('Helvetica', 11), background='white',foreground='black')
+style.configure('TEntry', font=('Helvetica', 11), background='white',foreground='black')
+style.configure('TLabel', font=('Helvetica', 11), background='white',foreground='black')
 
 
 def download():
@@ -28,7 +43,7 @@ def download_video(link):
     # pip install pytube3
     yt = YouTube(link)
     ys = yt.streams.get_highest_resolution()
-    ys.download('C:\\Users\\manoj\\Desktop\\folder')
+    ys.download(directory)
 
 
 def download_thumb(link):
@@ -38,7 +53,7 @@ def download_thumb(link):
     r = requests.get(url, stream=True)
     r.raw.decode_content = True
     # Open a local file with wb ( write binary ) permission.
-    with open("C:\\Users\\manoj\\Desktop\\folder\\{}".format(filename), 'wb') as f:
+    with open(directory.format(filename), 'wb') as f:
         shutil.copyfileobj(r.raw, f)
 
 
@@ -46,7 +61,7 @@ def download_desc(link):
     yt = YouTube(link)
     desc = yt.description
     title = yt.title.split(" ")
-    f = open("C:\\Users\\manoj\\Desktop\\folder\\{}.txt".format(
+    f = open(directory.format(
         title[0]), "w+")
     for i in desc:
         f.write(i)
@@ -54,19 +69,17 @@ def download_desc(link):
 
 
 # Adding youtubelogo
-path = "assets\\download.png"
+path = "assets/download.png"
 img = ImageTk.PhotoImage(PIL.Image.open(path))
-logo = Label(dwnld, image=img)
+logo = ttk.Label(dwnld, image=img)
 logo.grid(row=0, column=1, padx=(50, 50), pady=(20, 0))
 
 # entry link
-label0 = Label(dwnld, text="Paste the link here:", bg='white',
-               font='Helvetica').grid(row=1, column=1)
-entry_lnk = Text(dwnld, width=20, height=1)
+label0 = ttk.Label(dwnld, text="Paste the link here:").grid(row=1, column=1)
+entry_lnk = ttk.Entry(dwnld)
 entry_lnk.grid(row=2, column=1)
 
-label1 = Label(dwnld, text="Choose what to downlod :", bg='white',
-               font='Helvetica').grid(row=3, column=1)
+label1 = ttk.Label(dwnld, text="Choose what to downlod :").grid(row=3, column=1)
 # Adding combobox drop down list
 dwnld_format = StringVar()
 drop_dwn = ttk.Combobox(dwnld, width=27, height=40, textvariable=dwnld_format)
@@ -75,7 +88,6 @@ drop_dwn.grid(row=4, column=1, padx=(0, 0), pady=(10, 10))
 drop_dwn.current(0)
 
 # download button
-dwnld_button = Button(dwnld, text="Download", font='Helvetica', bg='red',
-                      fg='black', height=1, width=8, command=lambda: download()).grid(row=5, column=1)
+dwnld_button = ttk.Button(dwnld, text="Download" ,command=lambda: download()).grid(row=5, column=1)
 
 dwnld.mainloop()
